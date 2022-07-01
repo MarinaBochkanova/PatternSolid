43dea180e27a23f242863942aebfb31777586986
MarinaBochkanova <101672466+MarinaBochkanova@users.noreply.github.com>
2022-07-01 17:46:32 +0300

    Update AddRemove.java

--- src/main/java/AddRemove.java
+++ src/main/java/AddRemove.java
@@ -1,7 +1,7 @@
 import Product.Product;
 
 public interface AddRemove { // Interface segregation principle логическое разделение функций, интерфейс добавляет и
-    // удаляет товары сортирует но не сортирует список.
+    // удаляет товары но не сортирует список.
 
     void addProduct(Product product);
     void removeProduct(int index);
